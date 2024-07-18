e637863ce3a56de12efc7c40ed49817d227bdf21
yongen.loong <yongen.loong@aelf.io>
2024-07-18 17:30:01 +0800

    add cli commands

--- components/cli.tsx
+++ components/cli.tsx
@@ -7,6 +7,14 @@ export default function Cli() {
   const commands = {
     whoami: "jackharper",
     cd: (directory: string) => `changed path to ${directory}`,
+    help: () => (
+      <div>
+        <p>These are the available commands:</p>
+        <ol>
+          <li className="ml-8">clear - clears the terminal</li>
+        </ol>
+      </div>
+    ),
   };
 
   const { theme, systemTheme } = useTheme();
@@ -17,6 +25,12 @@ export default function Cli() {
       prompt="#"
       theme={theme !== "system" ? theme : systemTheme}
       showControlBar={false}
+      welcomeMessage={
+        <div>
+          <p>Welcome to AElf Playground.</p>
+          <p>Type help to see all commands.</p>
+        </div>
+      }
     />
   );
 }
