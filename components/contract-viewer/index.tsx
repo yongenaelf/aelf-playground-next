025e0f213a113f9981de692befef1c479e8b97f4
yongen.loong <yongen.loong@aelf.io>
2024-11-16 21:05:09 +0800

    fix: reload after 1s

--- components/contract-viewer/index.tsx
+++ components/contract-viewer/index.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useWallet } from "@/data/wallet";
 import { ContractView } from "aelf-smartcontract-viewer";
 import { useTheme } from "next-themes";
-import { Button } from "../ui/button";
 
 const sideChainTestnetRpc = "https://tdvw-test-node.aelf.io";
 
@@ -16,25 +15,26 @@ const ContractViewer = ({ name }: { name: string }) => {
   const contractViewerAddress = searchParams.get("contract-viewer-address");
   const [key, setKey] = useState("0");
 
+  useEffect(() => {
+    setTimeout(() => {
+      setKey(prev => parseInt(prev) + 1 + "");
+    }, 1000);
+  }, [contractViewerAddress]);
+
   if (!contractViewerAddress || !wallet?.wallet) {
     return;
   }
 
   return (
-    <>
-      <div className="flex">
-        <Button className="mx-auto mt-2" onClick={() => setKey(prev => parseInt(prev) + 1 + "")}>Refresh</Button>
-      </div>
-      <ContractView
-        key={key}
-        wallet={wallet.wallet}
-        address={contractViewerAddress}
-        headerTitle={"Contract View"}
-        rpcUrl={sideChainTestnetRpc}
-        contractName={name}
-        theme={resolvedTheme as "dark" | "light"}
-      />
-    </>
+    <ContractView
+      key={key}
+      wallet={wallet.wallet}
+      address={contractViewerAddress}
+      headerTitle={"Contract View"}
+      rpcUrl={sideChainTestnetRpc}
+      contractName={name}
+      theme={resolvedTheme as "dark" | "light"}
+    />
   );
 };
 
